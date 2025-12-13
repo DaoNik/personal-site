0b12eb1c2267b04ed868c5b90fddc5dbe3b22f30
DaoNik <nikitataranin@gmail.com>
2025-12-13 18:36:08 +0300

    fix: update app tests

--- client/src/app/app.spec.ts
+++ client/src/app/app.spec.ts
@@ -2,9 +2,17 @@ import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+  let component: App;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
     }).compileComponents();
+
+    component = TestBed.createComponent(App).componentInstance;
+  });
+
+  it('should be create App', () => {
+    expect(component).toBeTruthy();
   });
 });
